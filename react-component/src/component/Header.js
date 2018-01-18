f4bf4f8fedeb39e2c549f7cf3213317864d9ebd9
cupeng <872345732@qq.com>
2018-01-18 13:21:05 +0800

    component

---
+++ react-component/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Header extends React.Component {
+	render() {
+		return (<header>
+			<h3>头部</h3>
+		</header>)
+	}
+}
+
+export default Header;
