5a37aca583592601546b3f6b147b60a529133ad8
cupeng <872345732@qq.com>
2018-02-16 18:30:33 +0800

    react-cNode

---
+++ react-cnode/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (<div>
+        <h2>Hello world</h2>
+      </div>);
+  }
+}
+
+export default App;
